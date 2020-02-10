ba7bdfdecbb585d60e61e657c1aa4dd827cbda65
sonadztux <nanda@anakdesa.id>
2020-02-10 23:37:38 +0700

    add sign up for umkm and influencer routes

--- core/urls.py
+++ core/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path
-from .views import index, project_all
+from .views import index, signup_umkm, signup_influencer, project_all, project_detail, influencer_all
 
 app_name = 'core'
 
 urlpatterns = [
     path('', index, name='index'),
 
-    path('project/all/', project_all, name='project-all'),
-    # path('project/detail/', project_detail, name='project-detail')
+    path('accounts/signup/umkm/', signup_umkm, name='signup_umkm'),
+    path('accounts/signup/influencer/', signup_influencer, name='signup_influencer'),
+
+    path('project/all/', project_all, name='projects'),
+    path('project/detail/', project_detail, name='project_detail'),
+
+    path('influencer/all/', influencer_all, name='influencers')
 ]
