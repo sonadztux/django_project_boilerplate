a9ea6ba140fab444c974ee53696cdfc465432aac
sonadztux <nanda@anakdesa.id>
2020-02-10 21:13:06 +0700

    set views for project_all route

--- core/views.py
+++ core/views.py
@@ -3,13 +3,10 @@ from django.shortcuts import render
 from .models import Project
 
 def index(request):
-    return render(request, 'eshopper/index.html')
+    return render(request, 'index.html')
 
-def project_list(request):
-    context = {
-        'projects': Project.objects.all()
-    }
-    return render(request, "home.html", context)
+def project_all(request):
+    return render(request, "project-list.html")
 
 def project_detail(request):
     return render(request, "product-page.html")
