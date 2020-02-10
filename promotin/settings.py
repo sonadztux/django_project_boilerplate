c87de386af23ef91ee651913b0e5ffa86613ff77
sonadztux <nanda@anakdesa.id>
2020-02-10 21:13:40 +0700

    change static folder

--- promotin/settings.py
+++ promotin/settings.py
@@ -58,7 +58,7 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_files')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
