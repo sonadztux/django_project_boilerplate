3443365971c0f570df418a7ad6b54ad373fc974f
sonadztux <nanda@anakdesa.id>
2020-02-10 23:38:02 +0700

    add sign up for umkm and influencer views

--- core/views.py
+++ core/views.py
@@ -3,10 +3,19 @@ from django.shortcuts import render
 from .models import Project
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'promotin/index.html')
+
+def signup_umkm(request):
+    return render(request, 'allauth/account/signup-umkm.html')
+
+def signup_influencer(request):
+    return render(request, 'allauth/account/signup-influencer.html')
 
 def project_all(request):
-    return render(request, "project-list.html")
+    return render(request, 'promotin/project-list.html')
 
 def project_detail(request):
-    return render(request, "product-page.html")
+    return render(request, 'promotin/product-page.html')
+
+def influencer_all(request):
+    return render(request, 'promotin/influencer-list.html')
